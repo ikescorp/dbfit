97b171e037912bd2030da46d1dde23151e20f4a5
ikescorp <ike.scorp@gmail.com>
2015-03-27 12:04:09 +0530

    Updating NameNormaliser to allow -
    Updating NameNormaliser to allow - so that it can be used to identify the overloaded procedure

--- dbfit-java/core/src/main/java/dbfit/util/NameNormaliser.java
+++ dbfit-java/core/src/main/java/dbfit/util/NameNormaliser.java
@@ -8,7 +8,7 @@ public class NameNormaliser {
         // utility classes should not be instanciated
     }
 
-    private static String replaceIllegalCharactersWithSpacesRegex = "[^a-zA-Z0-9_.#]";
+    private static String replaceIllegalCharactersWithSpacesRegex = "[^a-zA-Z0-9_\-.#]";
 
     private static String replaceIllegalCharacters(final String name) {
         return name.replaceAll(replaceIllegalCharactersWithSpacesRegex, "");
